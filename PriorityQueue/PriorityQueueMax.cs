8ae0908ef9c1965304f2b15861f814b5b3a1a9f3
jourdonnais <szcsani@gmail.com>
2017-12-30 13:03:38 +0100

    a file is removed

---
+++ PriorityQueue/PriorityQueueMax.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    /// <summary>
+    /// PriorityQueue implementation with max-heap representation
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class PriorityQueueMax<T>
+    {
+        T[] heap;
+        double[] priorities;
+
+        int count;
+        int capacity;
+
+        public PriorityQueueMax(int capacity = 10)
+        {
+            this.heap = new T[capacity];
+            this.priorities = new double[capacity];
+            this.count = 0;
+            this.capacity = capacity;
+        }
+
+        public T[] Heap
+        {
+            get { return this.heap; }
+        }
+
+        public double[] Priorities
+        {
+            get { return this.priorities; }
+        }
+
+        public T pop()
+        {
+            T node = heap[0];
+            swap(0, count - 1);
+            count -= 1;
+            heapify(0);
+
+            return node;
+        }
+
+        public void add(T item, double priority)
+        {
+            if (count >= capacity)
+                throw new Exception("Count exceeds the maximum capacity!");
+
+            int position = count++;
+            this.heap[position] = item;
+            this.priorities[position] = priority;
+            this.moveUp(position);
+        }
+
+        public void addArray(Tuple<T, double>[] items)
+        {
+            foreach (var pair in items)
+            {
+                // Item1 = Node, Item 2 = priority(double)
+                this.add(pair.Item1, pair.Item2);
+            }
+        }
+
+        private void swap(int index_a, int index_b)
+        {
+            T temp = heap[index_a];
+            heap[index_a] = heap[index_b];
+            heap[index_b] = temp;
+
+            //also swap the priorities
+            double prio = priorities[index_a];
+            priorities[index_a] = priorities[index_b];
+            priorities[index_b] = prio;
+        }
+
+        private int parent(int position)
+        {
+            return (int)((position - 1) / 2);
+        }
+
+        private int leftChild(int position)
+        {
+            return 2 * position + 1;
+        }
+
+        private int rightChild(int position)
+        {
+            return 2 * position + 2;
+        }
+
+        private void moveUp(int position)
+        {
+            while ((position > 0) && (priorities[parent(position)] < priorities[position]))
+            {
+                //Debug.WriteLine("entered while loop");
+                int original_parent_pos = parent(position);
+                swap(position, original_parent_pos);
+                position = original_parent_pos;
+            }
+        }
+
+        private void heapify(int i)
+        {
+            int right = this.rightChild(i);
+            int left = this.leftChild(i);
+            int max;
+
+            if (left < count && priorities[left] > priorities[i])
+                max = left;
+            else
+                max = i;
+
+            if (right < count && priorities[right] > priorities[max])
+                max = right;
+
+            if (max != i)
+            {
+
+                this.swap(max, i);
+                //this.swap(i, max);
+
+                heapify(max);
+            }
+
+        }
+
+        private void buildHeap()
+        {
+            for (int i = count / 2; i >= 0; i--)
+            {
+                this.heapify(i);
+            }
+        }
+
+        public int Count
+        {
+            get { return this.count; }
+        }
+
+        public void heapSort()
+        {
+            buildHeap();
+            for (int i = count - 1; i > 0; i--)
+            {
+                swap(0, i);
+                this.heapify(i);
+            }
+        }
+
+
+
+    }
+}
---
+++ PriorityQueue/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    class Program
+    {
+        class Node : IComparable<Node>
+        {
+            int cost;
+
+            public Node(int cost)
+            {
+                this.cost = cost;
+            }
+            public int CompareTo(Node other)
+            {
+                return Cost > other.Cost ? 1 : 0;
+            }
+
+            public int Cost
+            {
+                get { return this.cost; }
+            }
+
+            public override string ToString()
+            {
+                return this.cost.ToString();
+
+            }
+        }
+
+        /// <summary>
+        /// Meghatározza egy adott számból (indexből), hogy hányadik szinten van
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static int ComputeLevel(int n)
+        {
+            return (n < 0) ? -1 : Convert.ToInt32(Math.Floor(Math.Log(n + 1, 2)));
+        }
+
+        static void Hline(int len, char c = ' ')
+        {
+            for (int i = 0; i < len; i++)
+                Console.Write(c);
+        }
+
+        static void printArray<T>(T[] t, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(t[i] + " ");
+            }
+        }
+        static void printArrayAsBinaryTree<T>(T[] t, int count, int start_y = 0)
+        {
+            int len = count;
+            int totalLevels = ComputeLevel(len);
+            int currentLevel = 1;
+
+            for (int i = 0; i < len; i++)
+            {
+                currentLevel = ComputeLevel(i);
+                //Console.SetCursorPosition(Console.CursorLeft, start_y + currentLevel);
+
+                int Space_Left = Convert.ToInt32(Math.Pow(2, totalLevels - currentLevel));
+                int Space_BTWSiblings = Convert.ToInt32(Math.Pow(2, totalLevels - currentLevel + 1)); //space beetween siblings
+
+                //első elem ezen a szinten?
+                if (currentLevel != ComputeLevel(i - 1))
+                {
+                    //ha új szint, akkor sortörés
+                    Console.WriteLine();
+                    Hline(Space_Left - 1);
+                }
+
+                Console.Write(t[i].ToString());
+                Hline(Space_BTWSiblings - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            PriorityQueueMin<Node> PQmin = new PriorityQueueMin<Node>();
+            PriorityQueueMax<Node> PQmax = new PriorityQueueMax<Node>();
+
+
+            PQmax.addArray(new Tuple<Node, double>[5]
+                 /*{
+                     new Tuple<Node, double>(new Node(2), 2),
+                     new Tuple<Node, double>(new Node(1), 1),
+                     new Tuple<Node, double>(new Node(5), 5),
+                     new Tuple<Node, double>(new Node(3), 3),
+                     new Tuple<Node, double>(new Node(4), 4)
+                 }*/
+
+                 {
+                        new Tuple<Node, double>(new Node(2), 2),
+                        new Tuple<Node, double>(new Node(1), 1),
+                        new Tuple<Node, double>(new Node(5), 5),
+                        new Tuple<Node, double>(new Node(3), 3),
+                        new Tuple<Node, double>(new Node(4), 4)
+                }
+            );
+
+
+
+
+            do
+            {
+                Hline(25, '-');
+                Console.WriteLine("count: " + PQmax.Count);
+                printArray<double>(PQmax.Priorities, PQmax.Count);
+                Console.WriteLine();
+                printArrayAsBinaryTree<Node>(PQmax.Heap, PQmax.Count);
+                Console.WriteLine("\n\npop item: {0}\n", PQmax.pop());
+            }
+            while (PQmax.Count > 0);
+
+
+
+
+            //printArrayAsBinaryTree<double>(PQmax.Priorities, PQmax.Count);
+            /*
+
+            Console.WriteLine("\n\nAfter HeapSort: ");
+            PQ.heapSort();
+
+            PrintArrayAsBinaryTree<Node>(PQ.Heap, PQ.Count);
+            */
+
+            Console.ReadLine();
+        }
+
+    }
+}
