78ea15465c47539b7cb2281713c80b685dd48d20
jourdonnais <szcsani@gmail.com>
2017-12-30 13:19:39 +0100

    Created new file for Node class

---
+++ PriorityQueue/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    /// <summary>
+    /// Simple Node class.
+    /// </summary>
+    class Node : IComparable<Node>
+    {
+        int cost;
+
+        public Node(int cost)
+        {
+            this.cost = cost;
+        }
+        public int CompareTo(Node other)
+        {
+            return Cost > other.Cost ? 1 : Cost == other.Cost ? 0 : -1;
+        }
+
+        public int Cost
+        {
+            get { return this.cost; }
+        }
+
+        public override string ToString()
+        {
+            return this.cost.ToString();
+
+        }
+    }
+}
